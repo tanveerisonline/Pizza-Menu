f6b77c0e2cdf70189be4c32870ade4198aa1fdb9
tanveerisonline <tanveerisonline@gmail.com>
2023-08-21 13:32:54 +0530

    Add Starter Project code to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const date = `06 - 06 - 2023`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>${date}</h1>
     </div>
   );
 }
